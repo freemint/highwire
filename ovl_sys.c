82a54e2b5dfebb1c63d271f79aba5bcf349c8046
Bishop Fox <altf4@phx2600.org>
2003-02-27 13:17:44 +0000

    [2003-01-21] extended OVL_METH declaration; added struct array to hold data of loaded OVLs; signal handler to receive disappeared OVL processes; new kill_ovl() to remove one particular or all OVLs

--- ovl_sys.c
+++ ovl_sys.c
@@ -7,7 +7,7 @@
 * starting guts of OVL loading and handling system
 *
 */
-/*#include <aes.h>*/
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -16,77 +16,208 @@
 
 #elif defined (LATTICE)
 # include <dos.h>
+# include <mintbind.h>
 # include <basepage.h>
 # define BASPAG BASEPAGE
 
 #elif defined (__GNUC__)
-# include <osbind.h>
+# include <mintbind.h>
 # include <basepage.h>
 # define BASPAG BASEPAGE
 #endif
 
+#ifndef EINVFN
+# define EINVFN -32
+#endif
+
+#include "hw-types.h"
 #include "ovl_sys.h"
 
-/* structs in use to hold OVL values*/
 
-OVL_METH *ovl_methods;
+typedef struct {
+	BASPAG   * basepage;
+	OVL_METH * methods;
+	short      pid;
+	void     (*handler)(void*);
+}      OVL_SLOT;
+static OVL_SLOT slot[8] = {
+	{NULL,NULL,0,NULL},{NULL,NULL,0,NULL},{NULL,NULL,0,NULL},{NULL,NULL,0,NULL},
+	{NULL,NULL,0,NULL},{NULL,NULL,0,NULL},{NULL,NULL,0,NULL},{NULL,NULL,0,NULL}
+};
 
-BASPAG *ovl_basepage;
+
+/*------------------------------------------------------------------------------
+ * signal handler for child process exited (flags >= OF_CHLDPRC)
+*/
+static void sig_chld (long sig)
+{
+	long  l   = Pwaitpid (-1, 0, NULL);
+	short pid = l >>16;
+	short i;
+	printf ("SIGCHLD: %i,%04lX \n", pid, l & 0xFFFF);
+	for (i = 0; i < numberof(slot); i++) {
+		if (slot[i].pid == pid) {
+			if (slot[i].handler) {
+				(*slot[i].handler)(slot[i].methods);
+				slot[i].handler = NULL;
+			}
+			free (slot[i].basepage);
+			slot[i].basepage = NULL;
+			slot[i].methods  = NULL;
+			slot[i].pid = 0;
+			break;
+		}
+	}
+	(void)sig;
+}
+
 
 /* ----------------------------------------------------------------- *
  *  Load OVL - does the Pexecs and scans the OVL for the functions   *
  * ----------------------------------------------------------------- */
-static int
-load_ovl(char *ovl_name)
+OVL_METH *
+load_ovl (const char * ovl_name, void (*handler)(void*))
 {
-	long i;
-	long newsize;
-
-	ovl_basepage = (BASPAG *)Pexec(3,ovl_name,NULL,NULL);
-	if ((long)ovl_basepage < 0L) {
+	BASPAG   * ovl_basepage = (BASPAG *)Pexec(3,ovl_name,NULL,NULL);
+	OVL_METH * ovl_methods  = NULL;
+	short      pid          = 0;
+	
+	if ((long)ovl_basepage <= 0L) {
 		puts ("Pexec() failed");
-		return 0;
+		return NULL;
+	
+	} else {
+		union { char c[4]; long l; } magic = { { OVL_MAGIC } };
+		OVL_METH * p   = (OVL_METH*)ovl_basepage->p_dbase;
+		OVL_METH * end = p + ovl_basepage->p_dlen - sizeof(OVL_METH);
+		do if (p->magic.l == magic.l) {
+			long r = p->revision & 0x88888888uL;
+			if (p->revision & ((r >>1) | (r >>2))) {        /* check for BCD */
+				printf ("OVL: infalid revision %08lX\n", p->revision);
+			} else if (p->revision < OVL_REVISION) {        /* compare date */
+				printf ("OVL: too old %08lX\n", p->revision);
+			} else if (&p->magic.l != p->magic_l) {         /* additional check */
+				printf ("OVL: invalid format (0x%lX/0x%lX)\n",
+				        (long)&p->magic.l, (long)p->magic_l);
+			} else {
+				ovl_methods = p;
+			}
+			break;
+		} while (++p < end);
 	}
-/*	Pexec(4,NULL,ovl_basepage,NULL);*/
-
-	/* this calculation is based off of the Pexec() cookbook
-	 * newsize = basepage + text + data + bss + heap + stack
-	 * 
-	 * some values are taken directly from there.  Also this is
-	 * probably fairly PureC specific
-	 */
-	 
-	newsize = 0x100 + ovl_basepage->p_tlen + ovl_basepage->p_dlen
-			 + ovl_basepage->p_blen + 0x800 + 0x800;
-			 
-#ifdef __PUREC__
-	Mshrink(0,ovl_basepage,newsize);
-
-#else /* LATTICE && GCC */
-	Mshrink(ovl_basepage,newsize);
-#endif
-
-	for (i = 0; i < ovl_basepage->p_dlen; i++)
-	{
-		/* Search for OVL Magic in DATA area */
-		if(strncmp((char *)ovl_basepage->p_dbase + i,OVL_MAGIC,OVL_HDR_LEN)==0)
-		{
-			/* set function pointer structure */
-			ovl_methods = (OVL_METH *)((char*)ovl_basepage->p_dbase + i + OVL_HDR_LEN);
-			
-			return 1;
+printf("ovl: bp=%p ft=%p \n", ovl_basepage, ovl_methods);
+	
+	if (ovl_methods) {
+		if (ovl_methods->flags >= OF_THREAD) { /* not supported yet */
+			ovl_methods = NULL;
+		
+		} else if (!ovl_methods->flags) { /* OF_SIMPLE */
+			/* this calculation is based off of the Pexec() cookbook
+			 * newsize = basepage + text + data + bss + heap + stack
+			 * 
+			 * some values are taken directly from there.  Also this is
+			 * probably fairly PureC specific
+			 */
+			size_t newsize = 0x100 + ovl_basepage->p_tlen + ovl_basepage->p_dlen
+			               + ovl_basepage->p_blen + 0x800 + 0x800;
+		#ifdef __PUREC__
+			Mshrink(0,ovl_basepage,newsize);
+		#else /* LATTICE && GCC */
+			Mshrink(ovl_basepage,newsize);
+		#endif
+		
+		} else {
+			if (ovl_methods->flags == OF_CHLDPRC) {
+				pid = Pexec (104, NULL, ovl_basepage, NULL);
+				if (pid > 0) {
+					Psignal (20/*SIGCHLD*/, (long)sig_chld);
+				} else if (pid == -32) { /* EINVFN */
+					pid = 0;
+				} else {
+					ovl_methods = NULL;
+				}
+			}
+			if (!pid) { /* OF_STARTUP or 104 not available */
+				if (Pexec (4, NULL, ovl_basepage, NULL) < 0) {
+					ovl_methods = NULL;
+				}
+			}
 		}
-	 }
- 
-	return 0;
+	}
+	
+	if (!ovl_methods) {
+		free (ovl_basepage);
+	
+	} else {
+		short i;
+		for (i = 0; i < numberof(slot); i++) {
+			if (!slot[i].methods) {
+				slot[i].basepage = ovl_basepage;
+				slot[i].methods  = ovl_methods;
+				slot[i].pid      = pid;
+				slot[i].handler  = handler;
+				break;
+			}
+		}
+	}
+	
+	return ovl_methods;
 }
+
+
+/* ----------------------------------------------------------------- *
+ * ----------------------------------------------------------------- */
+void
+kill_ovl (void * this_N_all)
+{
+	long  mask = Psigblock (1uL << 20/*SIGCHLD*/);
+	short i;
+	
+	for (i = 0; i < numberof(slot); i++) {
+		if (slot[i].methods && (!this_N_all || this_N_all == slot[i].methods)) {
+			if (slot[i].pid > 0) {
+				long r = Pkill (slot[i].pid,  19/*SIGCONT*/);
+				printf("kill(%i,SIGCONT) = %li\n", slot[i].pid, r);
+				
+				if ((r = Pwaitpid (slot[i].pid, 1, NULL)) != 0) {
+					slot[i].pid = 0;
+				}
+				printf("waitpid(%i) = %i,%04lX \n",
+					    (short)(r >>16), (short)(r >>16), r & 0xFFFF);
+			}
+		}
+	}
+	for (i = 0; i < numberof(slot); i++) {
+		if (slot[i].methods && (!this_N_all || this_N_all == slot[i].methods)) {
+			if (slot[i].pid > 0) {
+				long r = Pkill (slot[i].pid,  1/*SIGHUP*/);
+				printf("kill(%i,SIGHUP) = %li\n", slot[i].pid, r);
+				
+				r = Pwaitpid (slot[i].pid, 0, NULL);
+				printf("waitpid() = %i,%04lX \n", (short)(r >>16), r & 0xFFFF);
+			
+			}
+			if (slot[i].handler) {
+				(*slot[i].handler)(slot[i].methods);
+				slot[i].handler = NULL;
+			}
+			free (slot[i].basepage);
+			slot[i].basepage = NULL;
+			slot[i].methods  = NULL;
+			slot[i].pid = 0;
+		}
+	}
+	Psigsetmask (mask);
+}
+
+
 
 /* ----------------------------------------------------------------- *
  * ovl_infos - just grabbing and displaying the return from the      *
  *             ovl_version() call.                                   *
  * ----------------------------------------------------------------- */
 static int
-display_ovl_infos(void)
+display_ovl_infos (OVL_METH * ovl_methods)
 {
 	struct ovl_info_t *ovl_data;
 
@@ -102,17 +233,17 @@ display_ovl_infos(void)
 
 int load_sampleovl(void)
 {
-	if(load_ovl("modules\\sample.ovl") == 0)
+	OVL_METH * ovl_methods = load_ovl ("modules\\sample.ovl", (void(*)(void*))0);
+	if (!ovl_methods) {
 		printf("No OVL MAGIC found\r\n");
-	else
-	{
-		/* If we are here we found the OVL_MAGIC hidden in array[] */
+	
+	} else { /* If we are here we found the OVL_MAGIC hidden in array[] */
 
 		/* let OVL do any init work it needs to do */
 		ovl_init();
 		
 		/* Grab and show OVL version data */
-		display_ovl_infos();
+		display_ovl_infos (ovl_methods);
 
 		/* Close the OVL */
 		ovl_free();
--- ovl_sys.h
+++ ovl_sys.h
@@ -7,16 +7,12 @@
 * aka baldrick@netset.com
 *
 */
-
 #ifndef __OVL_API__H__
 #define __OVL_API__H__
 
-/*--- Macro for prototyping ---*/
-#ifdef __PUREC__
-#  define ___CDECL cdecl
-#else
-#  define ___CDECL
-#endif
+
+#define OVL_REVISION 0x20030120ul
+
 
 /* unique header identification */
 /* Use which ever version you feel more comfortable with,
@@ -25,8 +21,23 @@
 */
 #define OVL_MAGIC		"WiRe"
 
-/* header identification length */
-#define OVL_HDR_LEN		4
+
+/*--- Module functions used by the client ---*/
+typedef struct ovl_methods_t
+{
+	union { char c[4]; long l;
+	}    magic;
+	long revision; /* interface revision date in ISO yyyymmdd  BCD */
+	long flags;    /* same basic information: */
+	#define OF_SIMPLE  0x0000uL /* nothing special to do */
+	#define OF_STARTUP 0x0001uL /* needs to get main() started, Pexec(4) */
+	#define OF_CHLDPRC 0x0002uL /* needs to be started as process, Pexec(104) */
+	#define OF_THREAD  0x0003uL
+	long               __CDECL (*ovl_init)   (void);
+	struct ovl_info_t *__CDECL (*ovl_version)(void);
+	long               __CDECL (*ovl_free)   (void);
+	long * magic_l;
+} OVL_METH;
 
 /*--- OVL information ---*/
 struct ovl_info_t
@@ -37,18 +48,22 @@ struct ovl_info_t
 	char *author;
 };
 
-/*--- Module functions used by the client ---*/
-typedef struct ovl_methods_t
-{
-  long ___CDECL (*ovl_init)(void);
-  struct ovl_info_t *___CDECL (*ovl_version)(void);
-  long ___CDECL (*ovl_free)(void);
-} OVL_METH;
-
-
 
 /* define OVL_MODULE when you are building an OVL */
-#ifndef OVL_MODULE
+#ifdef OVL_MODULE
+
+#define _ovl_head { OVL_MAGIC }, OVL_REVISION
+#define _ovl_tail ovl_init, ovl_version, ovl_free, &ovl_methods.magic.l
+
+/* shorthand for the OVL function table definition */
+#define OVL_DECL(flags) OVL_METH ovl_methods = { _ovl_head, flags, _ovl_tail }
+
+
+#else /* ! OVL_MODULE*/
+
+OVL_METH * load_ovl (const char * ovl_name, void (*handler)(void*));
+void       kill_ovl (void * this_N_all);
+
 
 /* ----------------------------------------------------------------- *
  *  ovl_init() called by client to initialize ovl					 *
