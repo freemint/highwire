a351139871fd1455c6cfcccf027252b65bd76bbd
Bishop Fox <altf4@phx2600.org>
2003-02-27 13:13:45 +0000

    [2003-01-21] bumped to 0.1.2 alpha

--- defs.h
+++ defs.h
@@ -3,8 +3,8 @@
 #define _HIGHWIRE_MAJOR_     0
 #define _HIGHWIRE_MINOR_     1
 #define _HIGHWIRE_REVISION_  1
-#define _HIGHWIRE_BETATAG_   "beta"
-#define _HIGHWIRE_VERSION_   "0.1.1"
+#define _HIGHWIRE_BETATAG_   "alpha"
+#define _HIGHWIRE_VERSION_   "0.1.2"
 
 #include "hw-types.h"   /* get base definitions */
 
