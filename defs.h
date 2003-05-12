c008bc0d02c80ad785f4a21a0bec3c5cdcacba41
Bishop Fox <altf4@phx2600.org>
2003-05-12 13:46:32 +0000

    updated forgotten revision number

--- defs.h
+++ defs.h
@@ -2,7 +2,7 @@
  */
 #define _HIGHWIRE_MAJOR_     0
 #define _HIGHWIRE_MINOR_     1
-#define _HIGHWIRE_REVISION_  1
+#define _HIGHWIRE_REVISION_  2
 #define _HIGHWIRE_BETATAG_   "beta"
 #define _HIGHWIRE_VERSION_   "0.1.2"
 
