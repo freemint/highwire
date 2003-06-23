f793c278fd591ccf3e761a99495b915df1740fe5
Bishop Fox <altf4@phx2600.org>
2003-06-23 19:19:27 +0000

    config parser reworked for faster startup

--- config.c
+++ config.c
@@ -10,14 +10,77 @@
 #include "hwWind.h"
 
 
+/*----------------------------------------------------------------------------*/
+#define FA(f,b,i)   (((long)f <<16) | (b ? 0x10 : 0x00) | (i ? 0x01 : 0x00))
+static void
+cfg_font (char * param, long arg)
+{
+	long n = atol (param);
+	if (n >= 1 && n <= 0xFFFF) {
+		fonts[(short)(arg >>16)][arg & 0x10 ? 1 : 0][arg & 0x01] = n;
+	}
+}
+
+/*----------------------------------------------------------------------------*/
+static void
+cfg_fntsize (char * param, long arg)
+{
+	long n = atol (param);
+	(void)arg;
+	if (n >= 8 && n <= 24) {
+		font_size = n;
+	}
+}
+
+/*----------------------------------------------------------------------------*/
+static void
+cfg_minsize (char * param, long arg)
+{
+	long n = atol (param);
+	(void)arg;
+	if (n >= 1 && n <= 24) {
+		font_minsize = n;
+	}
+}
+
+/*----------------------------------------------------------------------------*/
+static void
+cfg_infobar (char * param, long arg)
+{
+	long n = atol (param);
+	(void)arg;
+	hwWind_setup (HWWS_INFOBAR, n);
+}
+
+/*----------------------------------------------------------------------------*/
+static void
+cfg_localweb (char * param, long arg)
+{
+	(void)arg;
+	local_web = strdup (param);
+}
+
+/*----------------------------------------------------------------------------*/
+static void
+cfg_func (char * param, long arg)
+{
+	long n = atol (param);
+	if (n) {
+		typedef void (*gen_func)(void);
+		(*(gen_func)arg)();
+	}
+}
+
+
+/******************************************************************************/
 WORD
 read_config(char *fn)
 {
-	char l[HW_PATH_MAX], * p;
+	char l[HW_PATH_MAX], * p, * d;
 	FILE * fp = NULL;
 	
 	if ((p = getenv ("HOME")) != NULL && *p) {
-		char * d = (p[1] == ':' ? "\\" : "/");
+		d = (p[1] == ':' ? "\\" : "/");
 		p = strchr (strcpy (l, p), '\0');
 		if (p[-1] != *d) *(p++) = *d;
 		strcat (strcat (strcpy (p, "defaults"), d), _HIGHWIRE_CFG_);
@@ -37,81 +100,68 @@ read_config(char *fn)
 	while ((p = fgets (l, (int)sizeof(l), fp)) != NULL) {
 		char  buf[22];
 		short b = 0;
-		long  n;
+		
+		if ((d = strchr (p, '#')) == NULL) {
+			d = strchr (p, '\0');
+		}
+		while (d > p && isspace(d[-1])) {
+			d--;
+		}
+		*d = '\0';
+		if (!*p) continue;
 		
 		while (isspace(*p)) p++;
-		if (!*p || *p == '#') continue;
-		
 		do if (*p == '_') {
 			buf[b] = *(p++);
 		} else if (isalpha(*p)) {
 			buf[b] = toupper(*(p++));
 		} else {
 			break;
-		} while (++b < sizeof(buf));
+		} while (++b < sizeof(buf) -1);
+		buf[b] = '\0';
 		
 		while (isspace(*p)) p++;
-		if (!b || *(p++) != '=' || !*p) continue;
-		
-		n = atol (p);
-		
-		if (strncmp ("FONT_SIZE", buf, b) == 0) {
-			if (n >= 8 && n <= 24) font_size = n;
-		
-		} else if (strncmp ("FONT_MINSIZE", buf, b) == 0) {
-			if (n >= 1 && n <= 24) font_minsize = n;
-		
-		} else if (strncmp ("NORMAL", buf, b) == 0) {
-			if (n >= 1 && n <= 0xFFFF) fonts[normal_font][0][0] = n;
-		
-		} else if (strncmp ("ITALIC_NORMAL", buf, b) == 0) {
-			if (n >= 1 && n <= 0xFFFF) fonts[normal_font][0][1] = n;
-		
-		} else if (strncmp ("BOLD_NORMAL", buf, b) == 0) {
-			if (n >= 1 && n <= 0xFFFF) fonts[normal_font][1][0] = n;
-		
-		} else if (strncmp ("BOLD_ITALIC_NORMAL", buf, b) == 0) {
-			if (n >= 1 && n <= 0xFFFF) fonts[normal_font][1][1] = n;
-		
-		} else if (strncmp ("HEADER", buf, b) == 0) {
-			if (n >= 1 && n <= 0xFFFF) fonts[header_font][0][0] = n;
-		
-		} else if (strncmp ("ITALIC_HEADER", buf, b) == 0) {
-			if (n >= 1 && n <= 0xFFFF) fonts[header_font][0][1] = n;
-		
-		} else if (strncmp ("BOLD_HEADER", buf, b) == 0) {
-			if (n >= 1 && n <= 0xFFFF) fonts[header_font][1][0] = n;
-		
-		} else if (strncmp ("BOLD_ITALIC_HEADER", buf, b) == 0) {
-			if (n >= 1 && n <= 0xFFFF) fonts[header_font][1][1] = n;
-		
-		} else if (strncmp ("TELETYPE", buf, b) == 0) {
-			if (n >= 1 && n <= 0xFFFF) fonts[pre_font][0][0] = n;
-		
-		} else if (strncmp ("ITALIC_TELETYPE", buf, b) == 0) {
-			if (n >= 1 && n <= 0xFFFF) fonts[pre_font][0][1] = n;
-		
-		} else if (strncmp ("BOLD_TELETYPE", buf, b) == 0) {
-			if (n >= 1 && n <= 0xFFFF) fonts[pre_font][1][0] = n;
-		
-		} else if (strncmp ("BOLD_ITALIC_TELETYPE", buf, b) == 0) {
-			if (n >= 1 && n <= 0xFFFF) fonts[pre_font][1][1] = n;
-		
-		} else if (strncmp ("LOCAL_WEB", buf, b) == 0) {
-			while (isspace(*p)) p++;
-			if (*p) local_web = strdup (p);
-		
-		} else if (strncmp ("LOGGING", buf, b) == 0) {
-			if (n) menu_logging();
-		
-		} else if (strncmp ("NO_IMAGE", buf, b) == 0) {
-			if (n) menu_alt_text();
-		
-		} else if (strncmp ("FORCE_FRAMECTRL", buf, b) == 0) {
-			if (n) menu_frm_ctrl();
-		
-		} else if (strncmp ("INFOBAR", buf, b) == 0) {
-			hwWind_setup (HWWS_INFOBAR, n);
+		if (buf[0] && *(p++) == '=' && *p) {
+			struct {
+				const char * key;
+				void       (*func)(char*, long);
+				long         arg;
+			} cfg[] = {
+				{ "BOLD_HEADER",          cfg_font,    FA(header_font, 1, 0) },
+				{ "BOLD_ITALIC_HEADER",   cfg_font,    FA(header_font, 1, 1) },
+				{ "BOLD_ITALIC_NORMAL",   cfg_font,    FA(normal_font, 1, 1) },
+				{ "BOLD_ITALIC_TELETYPE", cfg_font,    FA(pre_font,    1, 1) },
+				{ "BOLD_NORMAL",          cfg_font,    FA(normal_font, 1, 0) },
+				{ "BOLD_TELETYPE",        cfg_font,    FA(pre_font,    1, 0) },
+				{ "FONT_MINSIZE",         cfg_minsize, 0 },
+				{ "FONT_SIZE",            cfg_fntsize, 0 },
+				{ "FORCE_FRAMECTRL",      cfg_func,    (long)menu_frm_ctrl   },
+				{ "HEADER",               cfg_font,    FA(header_font, 0, 0) },
+				{ "INFOBAR",              cfg_infobar, 0 },
+				{ "ITALIC_HEADER",        cfg_font,    FA(header_font, 0, 1) },
+				{ "ITALIC_NORMAL",        cfg_font,    FA(normal_font, 0, 1) },
+				{ "ITALIC_TELETYPE",      cfg_font,    FA(pre_font,    0, 1) },
+				{ "LOCAL_WEB",            cfg_localweb,0 },
+				{ "LOGGING",              cfg_func,    (long)menu_logging    },
+				{ "NORMAL",               cfg_font,    FA(normal_font, 0, 0) },
+				{ "NO_IMAGE",             cfg_func,    (long)menu_alt_text   },
+				{ "TELETYPE",             cfg_font,    FA(pre_font,    0, 0) }
+			};
+			short beg = 0;
+			short end = (short)numberof(cfg) - 1;
+			do {
+				short i = (end + beg) / 2;
+				int   k = strcmp (buf, cfg[i].key);
+				if (k > 0) {
+					beg = i + 1;
+				} else if (k) {
+					end = i - 1;
+				} else {
+					while (isspace(*p)) p++;
+					(*cfg[i].func) (p, cfg[i].arg);
+					break;
+				}
+			} while (beg <= end);
 		}
 	}
 	fclose (fp);
