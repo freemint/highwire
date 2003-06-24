5d7b0e86a0b766ca65b280368ff9cf23f69d65a0
Bishop Fox <altf4@phx2600.org>
2003-06-24 03:55:35 +0000

    moved Mime Type declaration over to mime.h

--- Loader.h
+++ Loader.h
@@ -4,35 +4,7 @@
 #define __LOADER_H__
 
 
-typedef enum {   /* Basic MIME types from RFC2045/2046 by the
-                  * "Network Working Group",
-                  * partly registered with IANA
-                 */
-	MIME_Unknown = 0,
-
-	MIME_TEXT = 0x1000,
-		MIME_TXT_PLAIN,   /* text/plain "*"                         */
-		MIME_TXT_HTML,    /* text/html  "*.htm,*.html,*.php,*.php?" */
-
-	MIME_IMAGE = 0x2000,
-		MIME_IMG_JPEG,    /* image/jpeg   "*.jpg,*.jpeg" */
-		MIME_IMG_GIF,     /* image/gif    "*.gif"        */
-		MIME_IMG_PNG,     /* image/png    "*.png"        */
-		MIME_IMG_X_XIMG,  /* image/x-ximg "*.img"        */
-
-	MIME_AUDIO = 0x3000,
-		MIME_AUD_BASIC,   /* audio/basic */
-
-	MIME_VIDEO = 0x4000,
-		MIME_VID_MPEG,    /* video/mpeg   "*.mpg" */
-
-	MIME_APPL = 0x5000,
-		MIME_APP_OCTET,   /* application/octet-stream         */
-		MIME_APP_PDF      /* application/pdf          "*.pdf" */
-} MIMETYPE;
-
-#define MIME_Major(t)   (t & 0xF000)
-#define MIME_Minor(t)   (t & 0x0FFF)
+#include "mime.h"
 
 
 typedef struct s_loader {
