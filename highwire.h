dc2c0a9e2c92c151901beaa249c1313efeb979ba
Bishop Fox <altf4@phx2600.org>
2004-05-31 20:08:34 +0000

    popdown entries sorted in top/down order

--- highwire.h
+++ highwire.h
@@ -1,50 +1,50 @@
+/* resource set indices for HIGHWIRE */
 
-#define MENUTREE           0 	/* TREE - MENU   */
-#define M_HISTORY          6 	/* G_TITLE in (#0) MENUTREE */
-#define M_ABOUT           10 	/* G_STRING in (#0) MENUTREE */
-#define M_OPEN            19 	/* G_STRING in (#0) MENUTREE */
-#define M_INFO            21 	/* G_STRING in (#0) MENUTREE */
-#define M_QUIT            23 	/* G_STRING in (#0) MENUTREE */
-#define M_RELOAD          25 	/* G_STRING in (#0) MENUTREE */
-#define M_W1252           27 	/* G_STRING in (#0) MENUTREE */
-#define M_I8859_2         28 	/* G_STRING in (#0) MENUTREE */
-#define M_I8859_15        29 	/* G_STRING in (#0) MENUTREE */
-#define M_UTF8            30 	/* G_STRING in (#0) MENUTREE */
-#define M_UTF16           31 	/* G_STRING in (#0) MENUTREE */
-#define M_UTF16LE         32 	/* G_STRING in (#0) MENUTREE */
-#define M_MACINTOSH       33 	/* G_STRING in (#0) MENUTREE */
-#define M_ATARI_SYS       34 	/* G_STRING in (#0) MENUTREE */
-#define M_ATARINVDI       35 	/* G_STRING in (#0) MENUTREE */
-#define M_FONT_INC        37 	/* G_STRING in (#0) MENUTREE */
-#define M_FONT_DEC        38 	/* G_STRING in (#0) MENUTREE */
-#define M_HIST_BEG        40 	/* G_STRING in (#0) MENUTREE */
-#define M_HIST_END        60 	/* G_STRING in (#0) MENUTREE */
-#define M_LOGGING         62 	/* G_STRING in (#0) MENUTREE */
-#define M_ALT_TEXT        63 	/* G_STRING in (#0) MENUTREE */
-#define M_FRM_CTRL        65 	/* G_STRING in (#0) MENUTREE */
+#define MENUTREE               0 /* menu */
+#define M_HISTORY              6 /* TITLE in tree MENUTREE */
+#define M_ABOUT               10 /* STRING in tree MENUTREE */
+#define M_OPEN                19 /* STRING in tree MENUTREE */
+#define M_INFO                21 /* STRING in tree MENUTREE */
+#define M_QUIT                23 /* STRING in tree MENUTREE */
+#define M_RELOAD              25 /* STRING in tree MENUTREE */
+#define M_W1252               27 /* STRING in tree MENUTREE */
+#define M_I8859_2             28 /* STRING in tree MENUTREE */
+#define M_I8859_15            29 /* STRING in tree MENUTREE */
+#define M_UTF8                30 /* STRING in tree MENUTREE */
+#define M_UTF16               31 /* STRING in tree MENUTREE */
+#define M_UTF16LE             32 /* STRING in tree MENUTREE */
+#define M_MACINTOSH           33 /* STRING in tree MENUTREE */
+#define M_ATARI_SYS           34 /* STRING in tree MENUTREE */
+#define M_ATARINVDI           35 /* STRING in tree MENUTREE */
+#define M_FONT_INC            37 /* STRING in tree MENUTREE */
+#define M_FONT_DEC            38 /* STRING in tree MENUTREE */
+#define M_HIST_BEG            40 /* STRING in tree MENUTREE */
+#define M_HIST_END            60 /* STRING in tree MENUTREE */
+#define M_LOGGING             62 /* STRING in tree MENUTREE */
+#define M_ALT_TEXT            63 /* STRING in tree MENUTREE */
+#define M_FRM_CTRL            65 /* STRING in tree MENUTREE */
 
-#define ABOUT              1 	/* TREE - DIALOG */
-#define ABOUTOK            1 	/* G_BUTTON in (#1) ABOUT */
+#define ABOUT                  1 /* form/dialog */
+#define ABOUTOK                1 /* BUTTON in tree ABOUT */
 
-#define URLINPUT           2 	/* TREE - DIALOG */
-#define URL_OK             2 	/* G_BUTTON in (#2) URLINPUT */
-#define URL_CANCEL         3 	/* G_BUTTON in (#2) URLINPUT */
-#define URL_FILE           4 	/* G_BUTTON in (#2) URLINPUT */
-#define URL_ED_BG          5 	/* G_IBOX in (#2) URLINPUT */
-#define URL_EDIT           6 	/* G_FBOXTEXT in (#2) URLINPUT */
+#define URLINPUT               2 /* form/dialog */
+#define URL_OK                 2 /* BUTTON in tree URLINPUT */
+#define URL_CANCEL             3 /* BUTTON in tree URLINPUT */
+#define URL_FILE               4 /* BUTTON in tree URLINPUT */
+#define URL_ED_BG              5 /* IBOX in tree URLINPUT */
+#define URL_EDIT               6 /* FBOXTEXT in tree URLINPUT */
 
-#define RPOPUP             3 	/* TREE - DIALOG */
-#define RPOP_BACK          1 	/* G_STRING in (#3) RPOPUP */
-#define RPOP_FORWARD       2 	/* G_STRING in (#3) RPOPUP */
-#define RPOP_RELOAD        3 	/* G_STRING in (#3) RPOPUP */
-#define RPOP_VIEWSRC       5 	/* G_STRING in (#3) RPOPUP */
-#define RPOP_INFO          7 	/* G_STRING in (#3) RPOPUP */
-#define RPOP_COPY          8 	/* G_STRING in (#3) RPOPUP */
-#define RPOP_SAVE          9 	/* G_STRING in (#3) RPOPUP */
+#define RPOPUP                 3 /* form/dialog */
+#define RPOP_BACK              1 /* STRING in tree RPOPUP */
+#define RPOP_FORWARD           2 /* STRING in tree RPOPUP */
+#define RPOP_RELOAD            3 /* STRING in tree RPOPUP */
+#define RPOP_VIEWSRC           5 /* STRING in tree RPOPUP */
+#define RPOP_INFO              7 /* STRING in tree RPOPUP */
 
-#define RLINKPOP           4 	/* TREE - DIALOG */
-#define RLINK_OPEN         1 	/* G_STRING in (#4) RLINKPOP */
-#define RLINK_NEW          2 	/* G_STRING in (#4) RLINKPOP */
-#define RLINK_SAVE         3 	/* G_STRING in (#4) RLINKPOP */
-#define RLINK_INFO         5 	/* G_STRING in (#4) RLINKPOP */
-#define RLINK_COPY         6 	/* G_STRING in (#4) RLINKPOP */
+#define RLINKPOP               4 /* form/dialog */
+#define RLINK_OPEN             1 /* STRING in tree RLINKPOP */
+#define RLINK_NEW              2 /* STRING in tree RLINKPOP */
+#define RLINK_SAVE             3 /* STRING in tree RLINKPOP */
+#define RLINK_COPY             5 /* STRING in tree RLINKPOP */
+#define RLINK_INFO             7 /* STRING in tree RLINKPOP */
+
