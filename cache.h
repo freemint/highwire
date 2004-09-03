195590fd91e1a24440edbd44556cb1b18429d5e8
Bishop Fox <altf4@phx2600.org>
2004-09-03 20:04:05 +0000

    comments added

--- cache.h
+++ cache.h
@@ -2,6 +2,26 @@
 #define __CACHE_H__
 
 
+/*--- general setup and information ------------------------------------------*/
+
+void         cache_setup   (const char * dir, size_t mem_max,
+                            size_t dsk_max, size_t dsk_num);
+                /* Sets the cache directory and limits.  Arguments might be null
+                 * to stay unchanged.
+                 * dir:     absolute path where cache files get stored.
+                 * dsk_max: maximum amount of bytes used on disk.
+                 * dsk_num: maximum number of files to be held in disk cache.
+                 * mem_max: maximum for objects held in memory (normally
+                 *          images), affects only objects not in use.
+                */
+const char * cache_DirInfo(void);
+                /* Returns the absolute path of the cache directory (or NULL) as
+                 * set by cache_setup().
+                */
+
+/*--- - - - - - - - - - - ---*/
+
+
 typedef void       * CACHEOBJ;
 typedef const void * CACHED;
 
@@ -11,12 +31,6 @@ CACHED   cache_lookup  (LOCATION, long ident, long * opt_found);
 CACHED   cache_bound   (CACHED, LOCATION * exchange);
 CACHEOBJ cache_release (CACHED *, BOOL erase);
 size_t   cache_clear   (CACHED this_n_all);
-
-void     cache_setup   (const char * dir, size_t mem_max,
-                        size_t dsk_max, size_t dsk_num);
-
-const char * cache_DirInfo(void);
-
 
 typedef enum {
 	CR_NONE  = 0,
