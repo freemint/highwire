790e146931d4727fed34c8e4ac4eceeed7b8148a
Bishop Fox <altf4@phx2600.org>
2005-05-24 20:24:56 +0000

    code reworked for better readability and more coding safety

--- dl_mngr.c
+++ dl_mngr.c
@@ -15,16 +15,14 @@
 #include "highwire.h"
 #include "inet.h"
 
-static OBJECT * form = NULL;
-static WINDOW window = NULL;
+static OBJECT * dlm_form = NULL;
+static WINDOW   dlm_wind = NULL;
 
 
 #define BUF_SIZE 8192ul
 
 typedef struct s_slot {
-	const struct {
-		WORD Box, File, Info, Bar, Btn;
-	} F;
+	const WORD Base, File, Info, Pbar, Bttn;
 	struct {
 		WORD   Socket;
 		WORD   Target;
@@ -35,34 +33,63 @@ typedef struct s_slot {
 	} Data;
 } * SLOT;
 static struct s_slot slot_tab[4] = {
-	{ { DLM_1, DLM_1FILE, DLM_1TEXT, DLM_1BAR, DLM_1BTN }, },
-	{ { DLM_2, DLM_2FILE, DLM_2TEXT, DLM_2BAR, DLM_2BTN }, },
-	{ { DLM_3, DLM_3FILE, DLM_3TEXT, DLM_3BAR, DLM_3BTN }, },
-	{ { DLM_4, DLM_4FILE, DLM_4TEXT, DLM_4BAR, DLM_4BTN }, }
+	{ DLM_1, DLM_1FILE, DLM_1TEXT, DLM_1BAR, DLM_1BTN, },
+	{ DLM_2, DLM_2FILE, DLM_2TEXT, DLM_2BAR, DLM_2BTN, },
+	{ DLM_3, DLM_3FILE, DLM_3TEXT, DLM_3BAR, DLM_3BTN, },
+	{ DLM_4, DLM_4FILE, DLM_4TEXT, DLM_4BAR, DLM_4BTN, }
 };
-static char * btn_text_ok     = "-";
-static char * btn_text_stop   = "-";
-static char * btn_text_cancel = "-";
-static size_t file_text_max   = HW_PATH_MAX;
+#define slot_end   (slot_tab + numberof(slot_tab) -1)
+
+/*** some macros to make the code more handy ***/
+/* G_BOX: base box covering a slot */
+#define BASE_Flags(slot)   dlm_form[(slot)->Base].ob_flags
+/* G_STRING */
+#define FILE_Strng(slot)   dlm_form[(slot)->File].ob_spec.free_string
+#define FILE_StrLn(slot)   strlen (FILE_Strng(slot))
+/* G_TEXT */
+#define INFO_Ospec(slot)   dlm_form[(slot)->Info].ob_spec
+#define INFO_Strng(slot)   dlm_form[(slot)->Info].ob_spec.tedinfo->te_ptext
+#define INFO_StrLn(slot)   dlm_form[(slot)->Info].ob_spec.tedinfo->te_txtlen
+#define INFO_Adjst(slot)   dlm_form[(slot)->Info].ob_spec.tedinfo->te_just
+/* G_BOX: the progress bar */
+#define PBAR_Ospec(slot)   dlm_form[(slot)->Pbar].ob_spec
+#define PBAR_Flags(slot)   dlm_form[(slot)->Pbar].ob_flags
+#define PBAR_Fpatt(slot)   dlm_form[(slot)->Pbar].ob_spec.obspec.fillpattern
+#define PBAR_Color(slot)   dlm_form[(slot)->Pbar].ob_spec.obspec.interiorcol
+#define PBAR_Rect(slot)    (*(GRECT*)&dlm_form[(slot)->Pbar].ob_x)
+/* G_BUTTON */
+#define BTTN_State(slot)   dlm_form[(slot)->Bttn].ob_state
+#define BTTN_Strng(slot)   dlm_form[(slot)->Bttn].ob_spec.free_string
+
+static char * bttn_text_ok     = "-";
+static char * bttn_text_stop   = "-";
+static char * bttn_text_cancel = "-";
+static size_t file_text_max    = MAX_LEN; /* from gem.h */
+
+
+/*----------------------------------------------------------------------------*/
+#define slot_hide(slot)   {BASE_Flags(slot) |=  OF_HIDETREE;}
+#define slot_view(slot)   {BASE_Flags(slot) &= ~OF_HIDETREE;}
+#define slot_used(slot) (!(BASE_Flags(slot)  &  OF_HIDETREE))
 
 /*----------------------------------------------------------------------------*/
 static SLOT
 slot_find (void)
 {
 	SLOT slot = slot_tab;
-	while (!(form[slot->F.Box].ob_flags & OF_HIDETREE)) {
-		if (++slot >= &slot_tab[numberof(slot_tab)]) return NULL;
+	while (!(BASE_Flags(slot) & OF_HIDETREE)) {
+		if (++slot > slot_end) return NULL;
 	}
-	form[slot->F.Box].ob_flags &= ~OF_HIDETREE;
-	form[slot->F.Btn].ob_state &= ~OS_SELECTED;
-	form[slot->F.Info].ob_spec.tedinfo->te_just = TE_LEFT;
-	form[slot->F.Bar].ob_flags                  |= OF_HIDETREE;
-	form[slot->F.Bar].ob_spec.obspec.fillpattern = IP_SOLID;
-	form[slot->F.Bar].ob_spec.obspec.interiorcol = G_GREEN;
 	slot->Data.Socket = -1;
 	slot->Data.Target = -1;
 	slot->Data.Buffer = NULL;
 	slot->Data.Size   = -1;
+	slot_view (slot);
+	INFO_Adjst(slot) =  TE_LEFT;
+	PBAR_Flags(slot) |= OF_HIDETREE;
+	PBAR_Fpatt(slot) =  IP_SOLID;
+	PBAR_Color(slot) =  G_GREEN;
+	BTTN_State(slot) &= ~OS_SELECTED;
 	return slot;
 }
 
@@ -82,33 +109,28 @@ slot_remove (SLOT slot)
 		free (slot->Data.Buffer);
 		slot->Data.Buffer = NULL;
 	}
-	if (slot < &slot_tab[numberof(slot_tab)-1]) {
+	if (slot < slot_end) {
 		SLOT next = slot +1;
-		if (!(form[next->F.Box].ob_flags & OF_HIDETREE)) {
-			OBSPEC spec_file = form[slot->F.File].ob_spec;
-			OBSPEC spec_text = form[slot->F.Info].ob_spec;
-			OBSPEC spec_btn  = form[slot->F.Btn].ob_spec;
-			OBSPEC spec_bar  = form[slot->F.Bar].ob_spec;
-			GRECT  rect_bar  = *(GRECT*)&form[slot->F.Bar].ob_x;
+		if (slot_used (next)) {
+			char * file_text = FILE_Strng(slot);
+			OBSPEC info_spec = INFO_Ospec(slot);
+			char * bttn_text = BTTN_Strng(slot);
 			do {
-				form[slot->F.File].ob_spec = form[next->F.File].ob_spec;
-				form[slot->F.Info].ob_spec = form[next->F.Info].ob_spec;
-				form[slot->F.Btn].ob_spec  = form[next->F.Btn].ob_spec;
-				form[slot->F.Btn].ob_state = form[next->F.Btn].ob_state;
-				form[slot->F.Bar].ob_spec  = form[next->F.Bar].ob_spec;
-				*(GRECT*)&form[slot->F.Bar].ob_x = *(GRECT*)&form[next->F.Bar].ob_x;
-				slot->Data = next->Data;
-				slot       = next;
-			} while (++next < &slot_tab[numberof(slot_tab)]
-			         && !(form[next->F.Box].ob_flags & OF_HIDETREE));
-			form[slot->F.File].ob_spec = spec_file;
-			form[slot->F.Info].ob_spec = spec_text;
-			form[slot->F.Btn].ob_spec  = spec_btn;
-			form[slot->F.Bar].ob_spec  = spec_bar;
-			*(GRECT*)&form[slot->F.Bar].ob_x = rect_bar;
+				FILE_Strng(slot) = FILE_Strng(next);
+				INFO_Ospec(slot) = INFO_Ospec(next);
+				PBAR_Ospec(slot) = PBAR_Ospec(next);
+				PBAR_Rect(slot)  = PBAR_Rect(next);
+				BTTN_Strng(slot) = BTTN_Strng(next);
+				BTTN_State(slot) = BTTN_State(next);
+				slot->Data       = next->Data;
+				slot             = next;
+			} while (++next <= slot_end && slot_used (next));
+			FILE_Strng(slot) = file_text;
+			INFO_Ospec(slot) = info_spec;
+			BTTN_Strng(slot) = bttn_text;
 		}
-	}	
-	form[slot->F.Box].ob_flags |= OF_HIDETREE;
+	}
+	slot_hide (slot);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -117,7 +139,7 @@ slot_byArg (void * arg)
 {
 	SLOT slot = slot_tab;
 	while (slot->Data.Buffer != arg) {
-		if (++slot >= &slot_tab[numberof(slot_tab)]) return NULL;
+		if (++slot > slot_end) return NULL;
 	}
 	return slot;
 }
@@ -126,16 +148,15 @@ slot_byArg (void * arg)
 static void
 slot_error (SLOT slot, const char * text)
 {
-	strncpy (form[slot->F.Info].ob_spec.tedinfo->te_ptext, text,
-	         form[slot->F.Info].ob_spec.tedinfo->te_txtlen -1);
-	form[slot->F.Info].ob_spec.tedinfo->te_just = TE_LEFT;
-	form[slot->F.Btn].ob_state           &= ~OS_DISABLED;
-	form[slot->F.Btn].ob_spec.free_string = btn_text_cancel;
-	form[slot->F.Bar].ob_flags                  &= ~OF_HIDETREE;
-	form[slot->F.Bar].ob_spec.obspec.interiorcol = G_RED;
+	strncpy (INFO_Strng(slot), text, INFO_StrLn(slot) -1);
+	INFO_Adjst(slot) =  TE_LEFT;
+	BTTN_State(slot) &= ~OS_DISABLED;
+	BTTN_Strng(slot) =  bttn_text_cancel;
+	PBAR_Flags(slot) &= ~OF_HIDETREE;
+	PBAR_Color(slot) =  G_RED;
 	if (slot->Data.Size < 0) {
-		form[slot->F.Bar].ob_x     = 0;
-		form[slot->F.Bar].ob_width = 256;
+		PBAR_Rect(slot).g_x = 0;
+		PBAR_Rect(slot).g_w = 256;
 	}
 }
 
@@ -144,11 +165,11 @@ static void
 slot_redraw (SLOT slot, BOOL only_this)
 {
 	GRECT clip;
-	clip.g_x = form[ROOT].ob_x;
-	clip.g_w = form[ROOT].ob_width;
-	clip.g_y = form[ROOT].ob_y + form[slot->F.Box].ob_y;
-	clip.g_h = form[only_this ? slot->F.Box : ROOT].ob_height;
-	window_redraw (window, &clip);
+	clip.g_x = dlm_form[ROOT].ob_x;
+	clip.g_w = dlm_form[ROOT].ob_width;
+	clip.g_y = dlm_form[ROOT].ob_y + dlm_form[slot->Base].ob_y;
+	clip.g_h = dlm_form[only_this ? slot->Base : ROOT].ob_height;
+	window_redraw (dlm_wind, &clip);
 }
 
 
@@ -180,20 +201,20 @@ recv_job (void * arg, long invalidated)
 				if (slot->Data.Size >= 0) {
 					WORD w = ((slot->Data.Fill + slot->Data.Blck) *256)
 					       / slot->Data.Size;
-					if (w > form[slot->F.Bar].ob_width) {
-						dr_x = form[slot->F.Bar].ob_width;
+					if (w > PBAR_Rect(slot).g_w) {
+						dr_x = PBAR_Rect(slot).g_w;
 						dr_w = w - dr_x;
-						form[slot->F.Bar].ob_width = w;
-						draw = slot->F.Bar;
+						PBAR_Rect(slot).g_w = w;
+						draw = slot->Pbar;
 					}
 				} else {
-					if (form[slot->F.Bar].ob_x < 232) {
-						dr_x = form[slot->F.Bar].ob_x++;
+					if (PBAR_Rect(slot).g_x < 232) {
+						dr_x = PBAR_Rect(slot).g_x++;
 						dr_w = 25;
 					} else {
-						form[slot->F.Bar].ob_x = 0;
+						PBAR_Rect(slot).g_x = 0;
 					}
-					draw = form[slot->F.Bar].ob_next;
+					draw = dlm_form[slot->Pbar].ob_next;
 				}
 				save = (slot->Data.Blck > BUF_SIZE /2);
 			}
@@ -219,33 +240,33 @@ recv_job (void * arg, long invalidated)
 		
 		if (slot->Data.Size < 0) {
 			slot->Data.Size = slot->Data.Fill;
-			sprintf (form[slot->F.Info].ob_spec.tedinfo->te_ptext,
-			         "%li bytes", slot->Data.Size);
-			form[slot->F.Bar].ob_x = 0;
+			sprintf (INFO_Strng(slot), "%li bytes", slot->Data.Size);
+			PBAR_Rect(slot).g_x = 0;
 		}
 		if (slot->Data.Size == slot->Data.Fill) {
-			form[slot->F.Btn].ob_spec.free_string = btn_text_ok;
-			form[slot->F.Bar].ob_spec.obspec.fillpattern = IP_4PATT;
-			form[slot->F.Bar].ob_width                   = 256;
+			BTTN_Strng(slot)    = bttn_text_ok;
+			PBAR_Fpatt(slot)    = IP_4PATT;
+			PBAR_Rect(slot).g_w = 256;
 		} else {
 			slot_error (slot, "Connection broken!");
 		}
-		draw = slot->F.Box;
+/*		dlm_form[slot->Info].ob_spec.tedinfo->te_color |= G_LBLACK <<8;*/
+		draw = slot->Base;
 	}
 	if (draw) {
 		GRECT clip;
-		objc_offset (form, draw, &clip.g_x, &clip.g_y);
+		objc_offset (dlm_form, draw, &clip.g_x, &clip.g_y);
 		if (dr_w) {
 			clip.g_x += dr_x;
 			clip.g_w =  dr_w;
 		} else {
-			clip.g_w = form[draw].ob_width;
+			clip.g_w = dlm_form[draw].ob_width;
 		}
-		clip.g_h = form[draw].ob_height;
-		window_redraw (window, &clip);
+		clip.g_h = dlm_form[draw].ob_height;
+		window_redraw (dlm_wind, &clip);
 	}
 	if (slot->Data.Socket < 0) {
-		window_raise (window, TRUE, NULL);
+		window_raise (dlm_wind, TRUE, NULL);
 		return FALSE;
 	}
 	return TRUE;
@@ -289,22 +310,21 @@ fsel_job (void * arg, long invalidated)
 		err = TRUE;
 	
 	} else {
-		form[slot->F.Bar].ob_flags &= ~OF_HIDETREE;
-		form[slot->F.Bar].ob_x      = 0;
+		PBAR_Flags(slot)   &= ~OF_HIDETREE;
+		PBAR_Rect(slot).g_x = 0;
 		if ((slot->Data.Size = loader->DataSize) < 0) {
-			form[slot->F.Bar].ob_width = 24;
-			strcpy (form[slot->F.Info].ob_spec.tedinfo->te_ptext, "size unknown");
+			PBAR_Rect(slot).g_w = 24;
+			strcpy (INFO_Strng(slot), "size unknown");
 		} else {
-			form[slot->F.Bar].ob_width = 1;
-			sprintf (form[slot->F.Info].ob_spec.tedinfo->te_ptext,
-			         "%li bytes", slot->Data.Size);
+			PBAR_Rect(slot).g_w = 1;
+			sprintf (INFO_Strng(slot), "%li bytes", slot->Data.Size);
 		}
-		form[slot->F.Info].ob_spec.tedinfo->te_just = TE_RIGHT;
+		INFO_Adjst(slot) = TE_RIGHT;
 		ok  = TRUE;
 		err = FALSE;
 	}
 	slot_redraw (slot, TRUE);
-	window_raise  (window, TRUE, NULL);
+	window_raise  (dlm_wind, TRUE, NULL);
 	
 	while (ok) {
 		char fsel_file[HW_PATH_MAX];
@@ -360,7 +380,7 @@ fsel_job (void * arg, long invalidated)
 	}
 	if (ok) {
 		sched_insert (recv_job, slot->Data.Buffer, (long)slot, 20/*PRIO_RECIVE*/);
-		form[slot->F.Btn].ob_state &= ~OS_DISABLED;
+		BTTN_State(slot) &= ~OS_DISABLED;
 	} else if (!err) {
 		slot_remove (slot);
 	}
@@ -379,13 +399,12 @@ dlmngr_handler (OBJECT * tree, WORD obj)
 	(void)tree;
 	
 	if (obj < 0) {
-		if (form[slot_tab->F.Box].ob_flags & OF_HIDETREE) {
-			puts ("schwupps");
-			window = NULL; /* to be deleted */
+		if (!slot_used (slot_tab)) {
+			dlm_wind = NULL; /* to be deleted */
 		} else {
-			window_raise (window, FALSE, NULL);
+			window_raise (dlm_wind, FALSE, NULL);
 		}
-		return (window == NULL);;
+		return (dlm_wind == NULL);;
 	
 	} else {
 		SLOT slot = NULL;
@@ -400,9 +419,9 @@ dlmngr_handler (OBJECT * tree, WORD obj)
 				slot_remove (slot);
 				slot_redraw (slot, FALSE);
 			}
-			if (form[slot_tab->F.Box].ob_flags & OF_HIDETREE) {
-				free ((*window->destruct)(window));
-				window = NULL;
+			if (!slot_used (slot_tab)) {
+				free ((*dlm_wind->destruct)(dlm_wind));
+				dlm_wind = NULL;
 			}
 		} else {
 			printf ("dlmngr_handler: %i\n", obj);
@@ -416,34 +435,33 @@ dlmngr_handler (OBJECT * tree, WORD obj)
 void
 dl_manager (LOCATION loc, LOCATION ref)
 {
-	if (!window) {
-		if (!form) {
-			short dmy;
-			rsrc_gaddr (R_TREE, DLMNGR, &form);
-			form_center (form, &dmy,&dmy,&dmy,&dmy);
-			form[DLM_1].ob_flags |= OF_HIDETREE;
-			form[DLM_2].ob_flags |= OF_HIDETREE;
-			form[DLM_3].ob_flags |= OF_HIDETREE;
-			form[DLM_4].ob_flags |= OF_HIDETREE;
-			file_text_max = min (file_text_max,
-			                     strlen (form[DLM_1FILE].ob_spec.free_string));
-			file_text_max = min (file_text_max,
-			                     strlen (form[DLM_2FILE].ob_spec.free_string));
-			file_text_max = min (file_text_max,
-			                     strlen (form[DLM_3FILE].ob_spec.free_string));
-			file_text_max = min (file_text_max,
-			                     strlen (form[DLM_4FILE].ob_spec.free_string));
-			btn_text_ok     = form[DLM_1BTN].ob_spec.free_string;
-			btn_text_stop   = form[DLM_2BTN].ob_spec.free_string;
-			btn_text_cancel = form[DLM_3BTN].ob_spec.free_string;
+	if (!dlm_wind) {
+		short n;
+		if (!dlm_form) {
+			rsrc_gaddr (R_TREE, DLMNGR, &dlm_form);
+			form_center (dlm_form, &n,&n,&n,&n);
+			
+			/* store button texts */
+			bttn_text_ok     = BTTN_Strng (&slot_tab[0]);
+			bttn_text_stop   = BTTN_Strng (&slot_tab[1]);
+			bttn_text_cancel = BTTN_Strng (&slot_tab[2]);
+			
+			for (n = 0; n < numberof(slot_tab); n++) {
+				
+				/* determine maximum string length for file/url */
+				file_text_max = min (file_text_max, FILE_StrLn(&slot_tab[n]));
+				
+				/* no slots in use yet */
+				slot_hide (&slot_tab[n]);
+			}
 		}
-		window = window_byHandle (formwind_do (form, ROOT, "Download Manager",
-		                                       FALSE, dlmngr_handler));
+		n = formwind_do (dlm_form, 0, "Download Manager", FALSE, dlmngr_handler);
+		dlm_wind = window_byHandle (n);
 	} else {
-		window_raise (window, TRUE, NULL);
+		window_raise (dlm_wind, TRUE, NULL);
 	}
 	
-	if (window) {
+	if (dlm_wind) {
 		const char * host    = location_Host (loc, NULL);
 		const char   t_msk[] = "Connecting: %.*s";
 		LOADER       ldr;
@@ -453,13 +471,13 @@ dl_manager (LOCATION loc, LOCATION ref)
 			puts ("dl_manager(): no slot free");
 			return;
 		}
-		form[slot->F.Btn].ob_state |= OS_DISABLED;
-		form[slot->F.Btn].ob_spec.free_string = btn_text_stop;
-		sprintf (form[slot->F.Info].ob_spec.tedinfo->te_ptext, t_msk,
-		         (int)(form[slot->F.Info].ob_spec.tedinfo->te_txtlen -1
-		               -(sizeof(t_msk) -5)), (host ? host : "(???)"));
+		BTTN_State(slot) |= OS_DISABLED;
+		BTTN_Strng(slot) =  bttn_text_stop;
+		sprintf (INFO_Strng(slot), t_msk,
+		         (int)(INFO_StrLn(slot) -1 -(sizeof(t_msk) -5)),
+		         (host ? host : "(???)"));
 		if (!host) {
-			strcpy (form[slot->F.File].ob_spec.free_string, "???");
+			strcpy (FILE_Strng(slot), "???");
 		} else {
 			char buf[2048], * p = buf;
 			size_t len = location_FullName (loc, buf, sizeof(buf));
@@ -467,7 +485,7 @@ dl_manager (LOCATION loc, LOCATION ref)
 				p += len - file_text_max;
 				*p = '®';
 			}
-			strcpy (form[slot->F.File].ob_spec.free_string, p);
+			strcpy (FILE_Strng(slot), p);
 		}
 		slot_redraw (slot, TRUE);
 		
