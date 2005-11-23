a40d9d297afe59c9dda40448767bb48328d42ea6
baldrick <baldrick>
2005-11-23 15:38:38 +0000

    New rsc tree for images

--- highwire.h
+++ highwire.h
@@ -1,75 +1,83 @@
- /*  Resource C-Header-File v1.06 fÅr ResourceMaster v2.07 by ARDISOFT  */
 
-#define MENUTREE 0  /* menu */
-#define M_HISTORY 6  /* TITLE in tree MENUTREE */
-#define M_ABOUT 10  /* STRING in tree MENUTREE */
-#define M_OPEN 19  /* STRING in tree MENUTREE */
-#define M_INFO 21  /* STRING in tree MENUTREE */
-#define M_QUIT 23  /* STRING in tree MENUTREE */
-#define M_RELOAD 25  /* STRING in tree MENUTREE */
-#define M_W1252 27  /* STRING in tree MENUTREE */
-#define M_I8859_2 28  /* STRING in tree MENUTREE */
-#define M_I8859_15 29  /* STRING in tree MENUTREE */
-#define M_UTF8 30  /* STRING in tree MENUTREE */
-#define M_UTF16 31  /* STRING in tree MENUTREE */
-#define M_UTF16LE 32  /* STRING in tree MENUTREE */
-#define M_MACINTOSH 33  /* STRING in tree MENUTREE */
-#define M_ATARI_SYS 34  /* STRING in tree MENUTREE */
-#define M_ATARINVDI 35  /* STRING in tree MENUTREE */
-#define M_FONT_INC 37  /* STRING in tree MENUTREE */
-#define M_FONT_DEC 38  /* STRING in tree MENUTREE */
-#define M_HIST_BEG 40  /* STRING in tree MENUTREE */
-#define M_HIST_END 60  /* STRING in tree MENUTREE */
-#define M_COOKIES 62  /* STRING in tree MENUTREE */
-#define M_IMAGES 63  /* STRING in tree MENUTREE */
-#define M_USE_CSS 64  /* STRING in tree MENUTREE */
-#define M_FRM_CTRL 66  /* STRING in tree MENUTREE */
-#define M_LOGGING 67  /* STRING in tree MENUTREE */
+#define MENUTREE           0 	/* TREE - MENU   */
+#define M_HISTORY          6 	/* G_TITLE in (#0) MENUTREE */
+#define M_ABOUT           10 	/* G_STRING in (#0) MENUTREE */
+#define M_OPEN            19 	/* G_STRING in (#0) MENUTREE */
+#define M_INFO            21 	/* G_STRING in (#0) MENUTREE */
+#define M_QUIT            23 	/* G_STRING in (#0) MENUTREE */
+#define M_RELOAD          25 	/* G_STRING in (#0) MENUTREE */
+#define M_W1252           27 	/* G_STRING in (#0) MENUTREE */
+#define M_I8859_2         28 	/* G_STRING in (#0) MENUTREE */
+#define M_I8859_15        29 	/* G_STRING in (#0) MENUTREE */
+#define M_UTF8            30 	/* G_STRING in (#0) MENUTREE */
+#define M_UTF16           31 	/* G_STRING in (#0) MENUTREE */
+#define M_UTF16LE         32 	/* G_STRING in (#0) MENUTREE */
+#define M_MACINTOSH       33 	/* G_STRING in (#0) MENUTREE */
+#define M_ATARI_SYS       34 	/* G_STRING in (#0) MENUTREE */
+#define M_ATARINVDI       35 	/* G_STRING in (#0) MENUTREE */
+#define M_FONT_INC        37 	/* G_STRING in (#0) MENUTREE */
+#define M_FONT_DEC        38 	/* G_STRING in (#0) MENUTREE */
+#define M_HIST_BEG        40 	/* G_STRING in (#0) MENUTREE */
+#define M_HIST_END        60 	/* G_STRING in (#0) MENUTREE */
+#define M_COOKIES         62 	/* G_STRING in (#0) MENUTREE */
+#define M_IMAGES          63 	/* G_STRING in (#0) MENUTREE */
+#define M_USE_CSS         64 	/* G_STRING in (#0) MENUTREE */
+#define M_FRM_CTRL        66 	/* G_STRING in (#0) MENUTREE */
+#define M_LOGGING         67 	/* G_STRING in (#0) MENUTREE */
 
-#define ABOUT 1  /* form/dial */
-#define ABOUTOK 1  /* BUTTON in tree ABOUT */
+#define ABOUT              1 	/* TREE - DIALOG */
+#define ABOUTOK            1 	/* G_BUTTON in (#1) ABOUT */
 
-#define URLINPUT 2  /* form/dial */
-#define URL_OK 2  /* BUTTON in tree URLINPUT */
-#define URL_CANCEL 3  /* BUTTON in tree URLINPUT */
-#define URL_FILE 4  /* BUTTON in tree URLINPUT */
-#define URL_ED_BG 5  /* IBOX in tree URLINPUT */
-#define URL_EDIT 6  /* FBOXTEXT in tree URLINPUT */
+#define URLINPUT           2 	/* TREE - DIALOG */
+#define URL_OK             2 	/* G_BUTTON in (#2) URLINPUT */
+#define URL_CANCEL         3 	/* G_BUTTON in (#2) URLINPUT */
+#define URL_FILE           4 	/* G_BUTTON in (#2) URLINPUT */
+#define URL_ED_BG          5 	/* G_IBOX in (#2) URLINPUT */
+#define URL_EDIT           6 	/* G_FBOXTEXT in (#2) URLINPUT */
 
-#define RPOPUP 3  /* form/dial */
-#define RPOP_BACK 1  /* STRING in tree RPOPUP */
-#define RPOP_FORWARD 2  /* STRING in tree RPOPUP */
-#define RPOP_RELOAD 3  /* STRING in tree RPOPUP */
-#define RPOP_COPY 5  /* STRING in tree RPOPUP */
-#define RPOP_SAVE 6  /* STRING in tree RPOPUP */
-#define RPOP_VIEWSRC 8  /* STRING in tree RPOPUP */
-#define RPOP_INFO 10  /* STRING in tree RPOPUP */
+#define RPOPUP             3 	/* TREE - DIALOG */
+#define RPOP_BACK          1 	/* G_STRING in (#3) RPOPUP */
+#define RPOP_FORWARD       2 	/* G_STRING in (#3) RPOPUP */
+#define RPOP_RELOAD        3 	/* G_STRING in (#3) RPOPUP */
+#define RPOP_COPY          5 	/* G_STRING in (#3) RPOPUP */
+#define RPOP_SAVE          6 	/* G_STRING in (#3) RPOPUP */
+#define RPOP_VIEWSRC       8 	/* G_STRING in (#3) RPOPUP */
+#define RPOP_INFO         10 	/* G_STRING in (#3) RPOPUP */
 
-#define RLINKPOP 4  /* form/dial */
-#define RLINK_OPEN 1  /* STRING in tree RLINKPOP */
-#define RLINK_NEW 2  /* STRING in tree RLINKPOP */
-#define RLINK_SAVE 3  /* STRING in tree RLINKPOP */
-#define RLINK_COPY 5  /* STRING in tree RLINKPOP */
-#define RLINK_INFO 7  /* STRING in tree RLINKPOP */
+#define RLINKPOP           4 	/* TREE - DIALOG */
+#define RLINK_OPEN         1 	/* G_STRING in (#4) RLINKPOP */
+#define RLINK_NEW          2 	/* G_STRING in (#4) RLINKPOP */
+#define RLINK_SAVE         3 	/* G_STRING in (#4) RLINKPOP */
+#define RLINK_COPY         5 	/* G_STRING in (#4) RLINKPOP */
+#define RLINK_INFO         7 	/* G_STRING in (#4) RLINKPOP */
 
-#define DLMNGR 5  /* form/dial */
-#define DLM_1 1  /* IBOX in tree DLMNGR */
-#define DLM_1FILE 2  /* STRING in tree DLMNGR */
-#define DLM_1BAR 4  /* BOX in tree DLMNGR */
-#define DLM_1BTN 5  /* BUTTON in tree DLMNGR */
-#define DLM_1TEXT 6  /* TEXT in tree DLMNGR */
-#define DLM_2 7  /* IBOX in tree DLMNGR */
-#define DLM_2FILE 8  /* STRING in tree DLMNGR */
-#define DLM_2BAR 10  /* BOX in tree DLMNGR */
-#define DLM_2BTN 11  /* BUTTON in tree DLMNGR */
-#define DLM_2TEXT 12  /* TEXT in tree DLMNGR */
-#define DLM_3 13  /* IBOX in tree DLMNGR */
-#define DLM_3FILE 14  /* STRING in tree DLMNGR */
-#define DLM_3BAR 16  /* BOX in tree DLMNGR */
-#define DLM_3BTN 17  /* BUTTON in tree DLMNGR */
-#define DLM_3TEXT 18  /* TEXT in tree DLMNGR */
-#define DLM_4 19  /* IBOX in tree DLMNGR */
-#define DLM_4FILE 20  /* STRING in tree DLMNGR */
-#define DLM_4BAR 22  /* BOX in tree DLMNGR */
-#define DLM_4BTN 23  /* BUTTON in tree DLMNGR */
-#define DLM_4TEXT 24  /* TEXT in tree DLMNGR */
+#define DLMNGR             5 	/* TREE - DIALOG */
+#define DLM_1              1 	/* G_IBOX in (#5) DLMNGR */
+#define DLM_1FILE          2 	/* G_STRING in (#5) DLMNGR */
+#define DLM_1BAR           4 	/* G_BOX in (#5) DLMNGR */
+#define DLM_1BTN           5 	/* G_BUTTON in (#5) DLMNGR */
+#define DLM_1TEXT          6 	/* G_TEXT in (#5) DLMNGR */
+#define DLM_2              7 	/* G_IBOX in (#5) DLMNGR */
+#define DLM_2FILE          8 	/* G_STRING in (#5) DLMNGR */
+#define DLM_2BAR          10 	/* G_BOX in (#5) DLMNGR */
+#define DLM_2BTN          11 	/* G_BUTTON in (#5) DLMNGR */
+#define DLM_2TEXT         12 	/* G_TEXT in (#5) DLMNGR */
+#define DLM_3             13 	/* G_IBOX in (#5) DLMNGR */
+#define DLM_3FILE         14 	/* G_STRING in (#5) DLMNGR */
+#define DLM_3BAR          16 	/* G_BOX in (#5) DLMNGR */
+#define DLM_3BTN          17 	/* G_BUTTON in (#5) DLMNGR */
+#define DLM_3TEXT         18 	/* G_TEXT in (#5) DLMNGR */
+#define DLM_4             19 	/* G_IBOX in (#5) DLMNGR */
+#define DLM_4FILE         20 	/* G_STRING in (#5) DLMNGR */
+#define DLM_4BAR          22 	/* G_BOX in (#5) DLMNGR */
+#define DLM_4BTN          23 	/* G_BUTTON in (#5) DLMNGR */
+#define DLM_4TEXT         24 	/* G_TEXT in (#5) DLMNGR */
+
+#define RIMGPOP            6 	/* TREE - DIALOG */
+#define RIMG_OPEN          1 	/* G_STRING in (#6) RIMGPOP */
+#define RIMG_NEW           2 	/* G_STRING in (#6) RIMGPOP */
+#define RIMG_SAVE          3 	/* G_STRING in (#6) RIMGPOP */
+#define RIMG_COPY          5 	/* G_STRING in (#6) RIMGPOP */
+#define RIMG_INFO          7 	/* G_STRING in (#6) RIMGPOP */
+#define RIMG_SAVEIMG       9 	/* G_STRING in (#6) RIMGPOP */
+#define RIMG_COPYIMGURL   10 	/* G_STRING in (#6) RIMGPOP */
