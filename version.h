af2040f69a9c2918beecadf66f9f316d74f01f9f
baldrick <baldrick>
2006-02-08 18:54:36 +0000

    swiching version number to test

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
 #define _HIGHWIRE_MAJOR_     0
 #define _HIGHWIRE_MINOR_     3
-#define _HIGHWIRE_REVISION_  0
+#define _HIGHWIRE_REVISION_  10
 #define _HIGHWIRE_BETATAG_   ""
-#define _HIGHWIRE_VERSION_   "0.3.0"
+#define _HIGHWIRE_VERSION_   "0.3.10"
