fc2516d17ee54bfc2823fe93eea7f6c67d1d573e
Bishop Fox <altf4@phx2600.org>
2006-02-27 21:55:44 +0000

    new mvalid()

--- mem-diag.c
+++ mem-diag.c
@@ -11,7 +11,10 @@
 #include "hw-types.h"
 
 #define CHKBND /* enable memory region boundary check           */
-#undef SUMARY /* show orphaned memory blocks after program end */
+#define SUMARY /* show orphaned memory blocks after program end */
+
+
+LONG mvalid (void * mem);
 
 
 typedef struct s_mem_item * MEM_ITEM;
@@ -25,6 +28,12 @@ struct s_mem_item {
 static MEM_ITEM actv_lst = NULL;
 static MEM_ITEM free_lst = NULL;
 
+
+#ifdef CHKBND
+#define CANARY     8
+#else
+#define CANARY     0
+#endif
 
 /*----------------------------------------------------------------------------*/
 static inline ULONG ptr2offs (void * ptr)
@@ -44,8 +53,21 @@ mem_item (MEM_ITEM * plist, void * chunk)
 }
 
 /*----------------------------------------------------------------------------*/
+static MEM_ITEM *
+mem_find (MEM_ITEM * plist, void * chunk)
+{
+	while (*plist) {
+		if ((char*)chunk >= (char*)(*plist)->Chunk &&
+		    (char*)chunk <  (char*)(*plist)->Chunk + (*plist)->Size + CANARY *2) {
+			break;
+		}
+		plist = &(*plist)->Next;
+	}
+	return plist;
+}
+
+/*----------------------------------------------------------------------------*/
 #ifdef CHKBND
-#define CANARY     8
 #define CANARY_1   0xA5
 #define CANARY_4   0xA5A5A5A5uL
 #define CHUNKSIZE(s)   (s+CANARY*2)
@@ -166,15 +188,15 @@ malloc (size_t size)
 		MEM_ITEM * pitem = mem_item (&free_lst, chunk);	
 		if (*pitem) {
 			item = *pitem;
-/*			printf ("malloc(%li): %p duplette %li @<%05lX>.\n",
-			         size, CHUNK2MEM(chunk), item->Size,  ptr2offs (&size));
+/*			printf ("malloc(%li) @<%05lX>: %p duplicate %li.\n",
+			        size, ptr2offs(&size), CHUNK2MEM(chunk), item->Size);
 */			*pitem = item->Next;
 		} else if (!(item = __malloc (sizeof (struct s_mem_item)))) {
 			__free (chunk);
 			return NULL;
 		} else {
-/*			printf ("malloc(%li): %p @<%05lX>.\n",
-			         size, CHUNK2MEM(chunk), ptr2offs (&size));
+/*			printf ("malloc(%li) @<%05lX>: %p.\n",
+			         size, ptr2offs (&size), CHUNK2MEM(chunk));
 */		}
 	}
 #ifdef CHKBND
@@ -222,12 +244,12 @@ free (void * mem)
 #ifdef CHKBND
 		UWORD  patt = canary (item);
 		if (patt) {
-			printf ("free(%p): canary detected! @<%05lX>\n", mem, ptr2offs (&mem));
-			printf ("   %s created @<%05lX>\n",
+			printf ("free(%p) @<%05lX>: canary detected!\n", mem, ptr2offs (&mem));
+			printf ("   %s created @<%05lX>.\n",
 			        canary_str (patt, item->Size), item->Created);
 		}
 #endif
-/*		printf ("free(%p): %li @<%05lX>\n", mem, item->Size, ptr2offs (&mem));*/
+/*		printf ("free(%p) @<%05lX>: %li.\n", mem, ptr2offs (&mem)), item->Size;*/
 		*pitem        = item->Next;
 		item->Next    = free_lst;
 		free_lst      = item;
@@ -237,14 +259,70 @@ free (void * mem)
 		pitem = mem_item (&free_lst, chunk);
 		item  = *pitem;
 		if (item) {
-			printf ("free(%p): already deleted! @<%05lX>\n", mem, ptr2offs (&mem));
-			printf ("   %li bytes created  @<%05lX>, deleted @<%05lX>\n",
+			printf ("free(%p) @<%05lX>: already deleted!\n", mem, ptr2offs (&mem));
+			printf ("   %li bytes created @<%05lX>, deleted @<%05lX>.\n",
 			        item->Size, item->Created, item->Deleted);
 		} else {
 if (ptr2offs (&mem) < 0x3F000)  /* don't show known bugs in libungif */
-			printf ("free(%p): invalid pointer! @<%05lX>\n", mem, ptr2offs (&mem));
+			printf ("free(%p) @<%05lX>: invalid pointer!\n", mem, ptr2offs (&mem));
 		}
 	}
+}
+
+/*============================================================================*/
+LONG
+mvalid (void * mem)
+{
+	char     * chunk = MEM2CHUNK(mem);
+	MEM_ITEM * pitem = mem_item (&actv_lst, chunk);
+	MEM_ITEM   item  = *pitem;
+	if (item) {
+		/* everything is fine */
+		return 0;
+	}
+	pitem = mem_item (&free_lst, chunk);
+	item  = *pitem;
+	if (item) {
+		printf ("mvalid(%p) @<%05lX>: already deleted!\n",
+		        mem, ptr2offs (&mem));
+		printf ("   %li bytes created @<%05lX>, deleted @<%05lX>.\n",
+		        item->Size, item->Created, item->Deleted);
+		return -2;
+	}
+	pitem = mem_find (&actv_lst, chunk);
+	item  = *pitem;
+	if (!item && !*mem_find (&free_lst, chunk)) {
+		pitem = NULL;
+	}
+	if (pitem) {
+		const char * text;
+		long n = 0;
+		BOOL actv;
+		if (item) {
+			actv = TRUE;
+		} else {
+			actv = FALSE;
+			item = *pitem;
+		}
+		chunk = item->Chunk;
+		if ((n = (chunk + CANARY) - (char*)mem) > 0) {
+			text = "before";
+		} else if ((n = (char*)mem - (chunk + CANARY + item->Size -1)) > 0) {
+			text = "behind";
+		} else {
+			n    = (char*)mem - (chunk + CANARY);
+			text = "inside";
+		}
+		printf ("mvalid(%p) @<%05lX>:%s %li bytes %s %p\n",
+		        mem, ptr2offs (&mem), (actv ? "" : " invalid,"), n, text, chunk);
+		printf (actv ? "   %li bytes created @<%05lX>.\n"
+		             : "   %li bytes created @<%05lX>, deleted @<%05lX>.\n",
+		        item->Size, item->Created, item->Deleted);
+		return (actv ? -3 : -1);
+	}
+	printf ("mvalid(%p) @<%05lX>: invalid pointer!\n",
+	        mem, ptr2offs (&mem));
+	return -1;
 }
 
 #endif __GNUC__
