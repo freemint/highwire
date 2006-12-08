c2a13bff6f0b234ce91f0a8967eb3d932b4a8df1
baldrick <baldrick>
2006-12-08 21:37:41 +0000

    devl snapshot 0.3.1b9

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
 #define _HIGHWIRE_MAJOR_     0
 #define _HIGHWIRE_MINOR_     3
 #define _HIGHWIRE_REVISION_  1
-#define _HIGHWIRE_BETATAG_   "beta8"
+#define _HIGHWIRE_BETATAG_   "beta9"
 #define _HIGHWIRE_VERSION_   "0.3.1"
