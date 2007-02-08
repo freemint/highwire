0b6fd7d0412aab1ee0fb1ff0c5ea3363bab6aed5
baldrick <baldrick>
2007-02-08 21:48:30 +0000

    add in define for bookmark.css file

--- defs.h
+++ defs.h
@@ -8,6 +8,7 @@
 #define _HIGHWIRE_RSC_       "highwire.rsc"
 #define _HIGHWIRE_CFG_       "highwire.cfg"
 #define _HIGHWIRE_BOOKMARK_  "bookmark.htm"
+#define _HIGHWIRE_BKM_CSS_   "bookmark.css"
 
 #define _ERROR_SPEEDO_   "needs SpeedoGDOS or NVDI ò 3!"
 #define _ERROR_NOMEM_    "got no GLOBAL memory!"
