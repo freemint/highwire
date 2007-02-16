440743d57a8cf3d1ac221cc7e6aa30d5e8e866c2
baldrick <baldrick>
2007-02-16 21:06:07 +0000

    version 0.3.3 beta3

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
 #define _HIGHWIRE_MAJOR_     0
 #define _HIGHWIRE_MINOR_     3
 #define _HIGHWIRE_REVISION_  3
-#define _HIGHWIRE_BETATAG_   "á2"
+#define _HIGHWIRE_BETATAG_   "á3"
 #define _HIGHWIRE_VERSION_   "0.3.3"
