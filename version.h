095a8590e11a90393076a441fb9b5ce53b9ebf95
peter <peter>
2010-01-06 19:38:57 +0000

    version.h  PS - update beta version number

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
 #define _HIGHWIRE_MAJOR_     0
 #define _HIGHWIRE_MINOR_     3
 #define _HIGHWIRE_REVISION_  4
-#define _HIGHWIRE_BETATAG_   "á1"
+#define _HIGHWIRE_BETATAG_   "á3"
 #define _HIGHWIRE_VERSION_   "0.3.4"
